b6a4858224f109cad995edd062cd98308a7c8513
Guy Ardito <guyardito@gmail.com>
2020-08-09 14:27:20 -0400

    Add files via upload

---
+++ Quizzer/Shared/QuizzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizzerApp.swift
+//  Shared
+//
+//  Created by Guy Ardito on 8/9/20.
+//
+
+import SwiftUI
+
+@main
+struct QuizzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
